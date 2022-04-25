7c3abfd5b504ac957c52e9d9ff9f958e34b82b9d
Nathaniel Okenwa <nokenwa@twilio.com>
2022-04-25 11:10:25 +0100

    Fix Phone Mission

--- levels/vr_mission_template/objectives/make_phone_call/example.js
+++ levels/vr_mission_template/objectives/make_phone_call/example.js
@@ -5,8 +5,9 @@ const client = twilio(process.env.TQ_TWILIO_ACCOUNT_SID,process.env.TQ_TWILIO_AU
 client.calls.create({
   from: process.env.TQ_TWILIO_NUMBER,
   to:  //I wonder what would go here hmmmmm......
-  twiml: 
-
+  
+  url: 'https://handler.twilio.com/twiml/EH4e0d53cb8817cd80014c4156b07783c3' 
+  //The URL above points to a TWIML BIN
 
   // What is TWIML??? It's XML that tells Twilio what to do when the call is answerd
   // You will need a <Response> and a <Say> Tag
