ba1c83620b706c43741743698c36b9febab33946
Marius Obert <obatzer@gmail.com>
2022-07-28 11:25:45 +0200

    Add clear TODO annotations

--- levels/vr_mission_template/objectives/make_phone_call/example.js
+++ levels/vr_mission_template/objectives/make_phone_call/example.js
@@ -1,19 +1,18 @@
-const twilio = require('twilio')
+const twilio = require("twilio")
 const client = twilio(process.env.TQ_TWILIO_ACCOUNT_SID,process.env.TQ_TWILIO_AUTH_TOKEN);
 
 
 client.calls.create({
   from: process.env.TQ_TWILIO_NUMBER,
-  to: '', //I wonder what would go here hmmmmm......
-  url: //We need a URL that points to some TwiML' 
-
-  // What is TWIML??? It's XML that tells Twilio what to do when the call is answerd
-  // You will need a <Response> and a <Say> Tag
+  to: "", // TODO: Enter the number you want to call
+  url: ""// TODO: We need a URL that points to some TwiML
+  // What is TWIML??? It"s XML that tells Twilio what to do when the call is answerd
+  // You will need a <Response> and a <Say> Tag as explained here: https://www.twilio.com/docs/voice/twiml/say
 
 }).then(call => {
-  console.log('Woohoo! Copy this call SID into the hacking UI:');
+  console.log("Woohoo! Copy this call SID into the hacking UI:");
   console.log(`${call.sid}`);
 }).catch(error => {
-  console.error('Looks like the Twilio API returned an error:');
+  console.error("Looks like the Twilio API returned an error:");
   console.error(error);
 });
--- levels/vr_mission_template/objectives/send_message/example.js
+++ levels/vr_mission_template/objectives/send_message/example.js
@@ -1,18 +1,18 @@
-const twilio = require('twilio')
+const twilio = require("twilio")
 const client = twilio(process.env.TQ_TWILIO_ACCOUNT_SID,process.env.TQ_TWILIO_AUTH_TOKEN);
 
 
 client.messages.create({
   from: process.env.TQ_TWILIO_NUMBER,
 
-  // Hmm, this code will need two more parameters to work...
+  // TODO: This code will need two more parameters to work...
   // I wonder what those parameters are? Better check the docs:
   // https://www.twilio.com/docs/sms/api/message-resource
 
 }).then(message => {
-  console.log('Woohoo! Copy this message SID into the hacking UI:');
+  console.log("Woohoo! Copy this message SID into the hacking UI:");
   console.log(`${message.sid}`);
 }).catch(error => {
-  console.error('Looks like the Twilio API returned an error:');
+  console.error("Looks like the Twilio API returned an error:");
   console.error(error);
 });
