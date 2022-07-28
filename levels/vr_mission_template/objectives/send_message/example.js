5bfb846f44c2de7277b7a2714ce5c09ff23aa39d
Nathaniel Okenwa <nokenwa@twilio.com>
2022-07-28 14:30:13 +0100

    Delete messages from logs immediately

--- levels/vr_mission_template/objectives/send_message/example.js
+++ levels/vr_mission_template/objectives/send_message/example.js
@@ -9,6 +9,9 @@ client.messages.create({
   // I wonder what those parameters are? Better check the docs:
   // https://www.twilio.com/docs/sms/api/message-resource
 
+  // PRIVACY NOTICE: WE WILL NEVER USE YOUR NUMBER FOR ANYTHING ELSE.
+  // WE WILL DELETE IT FROM OUR LOGS IN 30 MINUTES
+
 }).then(message => {
   console.log('Woohoo! Copy this message SID into the hacking UI:');
   console.log(`${message.sid}`);
