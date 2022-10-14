72d54414a350544fed4c4aafaeae559d9406b258
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-10-13 23:31:23 -0700

    style(formatting): formatting

--- levels/vr_mission_template/events.js
+++ levels/vr_mission_template/events.js
@@ -1,42 +1,50 @@
+module.exports = function (event, world) {
+    console.log(`VR Template: ${event.name}`);
+    let electron = require("electron");
+    const levelFolder = `${electron.remote.app.getPath(
+        "userData"
+    )}/QuestIDE/${world.getCurrentLevelName()}`;
+    const sampleCodeFolder = `${
+        world.getContext().extensions.directory
+    }/twilioQuestBoothExecution/levels/vr_mission_template/objectives`;
+    const fs = require("fs");
 
-module.exports = function(event, world) {
-  console.log(`VR Template: ${event.name}`);
-  let electron = require('electron')
-  const levelFolder = `${electron.remote.app.getPath('userData')}/QuestIDE/${world.getCurrentLevelName()}`;
-  const sampleCodeFolder = `${world.getContext().extensions.directory}/twilioQuestBoothExecution/levels/vr_mission_template/objectives`;
-  const fs = require('fs');
+    require("dotenv").config();
 
+    if (
+        event.name === "triggerAreaWasEntered" &&
+        event.target.name.includes("Complete")
+    ) {
+        console.log(event);
+        console.log("Wiping Memory");
 
+        world.removeObjective("vr_mission_template", "tutorial");
+        world.removeObjective("vr_mission_template", "buy_number");
+        world.removeObjective("vr_mission_template", "send_message");
+        world.removeObjective("vr_mission_template", "make_phone_call");
+        world.removeObjective("vr_mission_template", "send_whatsapp");
+        world.removeObjective("vr_mission_template", "join_sandbox");
+        world.removeObjective("vr_mission_template", "swag_chest");
 
-  if (event.name === 'triggerAreaWasEntered' && event.target.name.includes('Complete')){
-    console.log(event)
-    console.log('Wiping Memory');
-  
-    world.removeObjective('vr_mission_template','tutorial')
-    world.removeObjective('vr_mission_template', 'buy_number')
-    world.removeObjective('vr_mission_template', 'send_message')
-    world.removeObjective('vr_mission_template', 'make_phone_call')
-    world.removeObjective('vr_mission_template', 'send_whatsapp')
-    world.removeObjective('vr_mission_template', 'join_sandbox')
-    world.removeObjective('vr_mission_template', 'swag_chest')
+        //RESET USER CODE
+        fs.readdir(levelFolder, (err, files) => {
+            files.forEach((missionFolder) => {
+                fs.readdir(`${levelFolder}/${missionFolder}`, (err, files) => {
+                    console.log(`Rewriting ${missionFolder}`);
+                    const dest = `${levelFolder}/${missionFolder}/user_code.js`;
+                    const src = `${sampleCodeFolder}/${missionFolder}/example.js`;
+                    fs.copyFile(
+                        src,
+                        dest,
+                        fs.constants.COPYFILE_FICLONE,
+                        (error) => console.error
+                    );
+                });
+            });
+        });
 
-    //RESET USER CODE
-    fs.readdir(levelFolder, (err, files) => {
-      files.forEach(missionFolder =>{
-        fs.readdir(`${levelFolder}/${missionFolder}`, (err,files) =>{
-        
-            console.log(`Rewriting ${missionFolder}`)
-            const dest = `${levelFolder}/${missionFolder}/user_code.js`
-            const src = `${sampleCodeFolder}/${missionFolder}/example.js`
-            fs.copyFile( src, dest, fs.constants.COPYFILE_FICLONE,(error)=> console.error)
-        })
-      });
-    });
+        world.warp("vr_mission_template", "player_entry1", "default");
 
-
-    world.warp('vr_mission_template','player_entry1','default')
-    
-    console.log(world);
-
-  }
-}
+        console.log(world);
+    }
+};
