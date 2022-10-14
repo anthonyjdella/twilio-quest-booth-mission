4b8882e31c77eb71264513f646770c7bc542c80b
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-10-13 23:31:02 -0700

    refactor(send_message): small refactoring of text

--- levels/vr_mission_template/objectives/send_message/description.md
+++ levels/vr_mission_template/objectives/send_message/description.md
@@ -1,5 +1,18 @@
 # SMS: Send Me Something!
 
-Using your own tools or the **code editor**, send an SMS message using the Twilio API. When a new Message is created via the API, it is assigned a unique identifier - a **SID**. This SID will be [included in the data sent back from a successful API request](https://www.twilio.com/docs/sms/api/message-resource#message-properties).
+<div class="aside">
+  <h3>To-Do List</h3>
+  <ul>
+    <li>Show Code Editor</li>
+    <li>Enter the 2 missing parameters</li>
+    <li>Enter the SID in the field on the right, then click <em>HACK</em>.</li>
+  </ul>
+</div>
+
+To complete this challenge, you will need to <strong>send a text message using the Twilio SMS API</strong>.
+
+Using the **Code Editor**, you will need to **fill in the 2 required missing parameters**. Think about what kind of information you need to send a text message. Or find the required parameters in the [docs](https://www.twilio.com/docs/sms/api/message-resource).
+
+When a new Message is created via the API, it is assigned a unique identifier - a SID. This SID will be [included in the data sent back from a successful API request](https://www.twilio.com/docs/sms/api/message-resource#message-properties).
 
 Retrieve a message SID after a successful API request and enter it into the text field on the right. When you hit *HACK*, we'll validate that this message SID exists in your Twilio account, and you will be victorious!
--- levels/vr_mission_template/objectives/send_message/example.js
+++ levels/vr_mission_template/objectives/send_message/example.js
@@ -1,21 +1,29 @@
-const twilio = require("twilio")
-const client = twilio(process.env.TQ_TWILIO_ACCOUNT_SID,process.env.TQ_TWILIO_AUTH_TOKEN);
+/***************************************************************
+    This mission uses the Javascript programming language.
 
+    Run this program by clicking the Play button.
+***************************************************************/
 
-client.messages.create({
-  from: process.env.TQ_TWILIO_NUMBER,
+const twilio = require("twilio");
+const client = twilio(
+    process.env.TQ_TWILIO_ACCOUNT_SID,
+    process.env.TQ_TWILIO_AUTH_TOKEN
+);
 
-  // TODO: This code will need two more parameters to work...
-  // I wonder what those parameters are? Better check the docs:
-  // https://www.twilio.com/docs/sms/api/message-resource
+client.messages
+    .create({
+        from: process.env.TQ_TWILIO_NUMBER,
+        // TODO: first missing parameter,
+        // TODO: second missing parameter
+    })
+    .then((message) => {
+        console.log(`SID: ${message.sid}`);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 
-  // PRIVACY NOTICE: WE WILL NEVER USE YOUR NUMBER FOR ANYTHING ELSE.
-  // WE WILL DELETE IT FROM OUR LOGS IN 30 MINUTES
-
-}).then(message => {
-  console.log("Woohoo! Copy this message SID into the hacking UI:");
-  console.log(`${message.sid}`);
-}).catch(error => {
-  console.error("Looks like the Twilio API returned an error:");
-  console.error(error);
-});
+/***************************************************************
+    PRIVACY NOTICE: WE WILL NEVER USE YOUR NUMBER FOR ANYTHING ELSE.
+    WE WILL DELETE IT FROM OUR LOGS IN 30 MINUTES
+***************************************************************/
