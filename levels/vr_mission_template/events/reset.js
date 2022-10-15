b8e1e0f6b3a5d964a000478c9f26a2bfd75eb409
Anthony_Dellavecchia <dev@anthonydellavecchia.com>
2022-10-14 23:50:50 -0700

    fix(reset): fixed code not resetting

--- levels/vr_mission_template/events/reset.js
+++ levels/vr_mission_template/events/reset.js
@@ -8,14 +8,6 @@ async function reset(event, world, worldState) {
     }/twilioQuestBoothExecution/levels/vr_mission_template/objectives`;
     const fs = require("fs");
 
-    //levelFolder=/Users/anthonyjdella/Library/Application Support/TwilioQuest/QuestIDE/fog_owl
-    //sampleCodeFolder=/Users/anthonyjdella/Desktop/Twilio-Quest/twilioQuestBoothExecution/levels/vr_mission_template/objectives
-
-    //levelFolder=/Users/anthonyjdella/Library/Application Support/TwilioQuest/QuestIDE/vr_mission_template
-
-    // console.log(event);
-    console.log("HEHE");
-    console.log(levelFolder, sampleCodeFolder);
     console.log("Wiping Memory");
 
     world.removeObjective("vr_mission_template", "tutorial");
@@ -27,19 +19,24 @@ async function reset(event, world, worldState) {
     world.removeObjective("vr_mission_template", "swag_chest");
 
     //RESET USER CODE
-    fs.readdir(levelFolder, (err, files) => {
+    fs.readdir(sampleCodeFolder, (err, files) => {
+        const boothMissionFolder =
+            "/Users/anthonyjdella/Library/Application Support/TwilioQuest/QuestIDE/vr_mission_template";
+
         files.forEach((missionFolder) => {
-            fs.readdir(`${levelFolder}/${missionFolder}`, (err, files) => {
-                console.log(`Rewriting ${missionFolder}`);
-                const dest = `${levelFolder}/${missionFolder}/user_code.js`;
-                const src = `${sampleCodeFolder}/${missionFolder}/example.js`;
-                fs.copyFile(
-                    src,
-                    dest,
-                    fs.constants.COPYFILE_FICLONE,
-                    (error) => console.error
-                );
-            });
+            fs.readdir(
+                `${boothMissionFolder}/${missionFolder}`,
+                (err, files) => {
+                    const dest = `${boothMissionFolder}/${missionFolder}/user_code.js`;
+                    const src = `${sampleCodeFolder}/${missionFolder}/example.js`;
+                    fs.copyFile(
+                        src,
+                        dest,
+                        fs.constants.COPYFILE_FICLONE,
+                        (error) => console.error
+                    );
+                }
+            );
         });
     });
 }
